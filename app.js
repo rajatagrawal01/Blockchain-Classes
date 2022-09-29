603a87fbb067d56b929e641e0108bc8ab834385a
avi4s <avisharma410@gmail.com>
2022-09-29 17:35:59 +0530

    avi

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+var indexroutes = require("./routes");
+const app = express();
+const session = require('express-session');
+
+const hbs = require('hbs');
+var http = require('http');
+var https = require('https');
+const cookieParser = require('cookie-parser');
+var fs = require('fs-extra');
+const routes = require('./routes/index.js');
+// const probit = require('./routes/probit.js');
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/AbuBakr",
+{ useUnifiedTopology: true } ,
+{ useNewUrlParser: true });
+//new api
+const flash = require('express-flash');
+
+
+app.use(session({
+    secret: 'userdetails',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+// Set public folder
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.use(flash())
+
+app.use("/", indexroutes);
+console.log(process.env.ADMIN)
+
+app.use(cookieParser('keyboard cat'));
+app.use(session({ 
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+
+app.get('*', function(req,res,next) {
+   res.locals.cart  = req.session.cart;
+   res.locals.user  = req.user || null;
+   next();
+});
+
+app.use('/', routes);
+
+ app.engine('ejs', require('ejs').renderFile);
+ app.set('view engine', 'ejs');
+
+ app.get('/', (req, res) => {
+    res.send('Hello World!')
+    res.setHeader('X-Foo', 'bar')
+   })
+
+
+console.log('3009')
+
+http.createServer(app).listen(4000); 
+// Create an HTTPS service identical to the HTTP service.
+// https.createServer(options, app).listen(443);
+
+
+ // Set 'views' directory for any views 
+ // being rendered res.render()
+const PORT = 443;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
